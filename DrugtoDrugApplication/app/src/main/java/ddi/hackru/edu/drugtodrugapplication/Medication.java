b2965c602c885c8d13847c605e990c4368018ce8
rushabhjhaveri <rjhaveri98@gmail.com>
2017-04-22 15:05:18 -0400

    Added Changes

--- DrugtoDrugApplication/app/src/main/java/ddi/hackru/edu/drugtodrugapplication/Medication.java
+++ DrugtoDrugApplication/app/src/main/java/ddi/hackru/edu/drugtodrugapplication/Medication.java
@@ -1,8 +1,45 @@
 package ddi.hackru.edu.drugtodrugapplication;
-
+import java.io.*;
+import java.util.*;
 /**
  * Created by Rushabh on 4/22/2017.
  */
 
 public class Medication {
+
+    //instance variable declarations
+
+    private String drugName;
+    private String rxNormID;
+    private List<Adversity> adversityList;
+
+    //default constructor
+    //initializes instance mem
+    Medication(){
+        drugName = null;
+        rxNormID = null;
+    }
+
+    //parametrized constructor
+    Medication(String drugName, String rxNormID){
+        this.drugName = drugName;
+        this.rxNormID = rxNormID;
+    }
+
+    //setter method - sets AdversityList to pre-made, sorted adversity list
+    public void setAdversityList(List<Adversity> adversityList) {
+        this.adversityList = adversityList;
+    }
+
+    public String getDrugName(){
+        return this.drugName;
+    }
+
+    public String getRXNormID(){
+        return this.rxNormID;
+    }
+
+    public List<Adversity> getAdversityList(){
+        return this.adversityList;
+    }
 }
