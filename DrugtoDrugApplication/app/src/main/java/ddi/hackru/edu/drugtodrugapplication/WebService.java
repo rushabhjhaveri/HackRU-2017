b33a6baaac00c8c1c344a48fdfd2419cf39b864f
Nemferno <jasperbae@gmail.com>
2017-04-22 23:27:21 -0400

    Added OCR Implementation

--- DrugtoDrugApplication/app/src/main/java/ddi/hackru/edu/drugtodrugapplication/WebService.java
+++ DrugtoDrugApplication/app/src/main/java/ddi/hackru/edu/drugtodrugapplication/WebService.java
@@ -1,15 +1,35 @@
 package ddi.hackru.edu.drugtodrugapplication;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.AsyncTask;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.xmlpull.v1.XmlPullParserException;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
 import java.util.List;
+
+import javax.net.ssl.HttpsURLConnection;
 
 /**
  * Created by Jasper Bae on 4/22/2017.
@@ -138,6 +158,50 @@ public class WebService
             }
         };
         task.execute(rxnormid);
+    }
+
+    public void requestPhotoRecognition(String imageToUrl)
+    {
+        AsyncTask<String, Void, Medication> task = new AsyncTask<String, Void, Medication>()
+        {
+            @Override
+            protected Medication doInBackground(String... params)
+            {
+                CloseableHttpClient httpClient = HttpClients.createDefault();
+
+                try
+                {
+                    URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/vision/v1.0/ocr");
+                    uriBuilder.setParameter("language", "unk");
+                    uriBuilder.setParameter("detectOrientation", "true");
+
+                    URI uri = uriBuilder.build();
+                    HttpPost post = new HttpPost(uri);
+
+                    post.addHeader("Content-Type", "application/octet-stream");
+                    post.addHeader("Ocp-Apim-Subscription-Key", "b2d7d2471e2142a79a2c4a6d3b9b0778");
+
+                    File file = new File(params[0]);
+                    FileEntity reqEntity = new FileEntity(file, ContentType.APPLICATION_OCTET_STREAM);
+                    post.setEntity(reqEntity);
+
+                    HttpResponse response = httpClient.execute(post);
+                    HttpEntity entity = response.getEntity();
+
+                    if(entity != null)
+                    {
+                        String r = EntityUtils.toString(entity);
+                        System.out.println("E:" + r);
+                    }
+                }catch(Exception e)
+                {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }
+        };
+        task.execute(imageToUrl);
     }
 
     public static WebService getInstance()
